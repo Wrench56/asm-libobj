1c733418ecbcc9988f7c9fae06b203117f2db22f
Mark Devenyi <markdevenyidev@gmail.com>
2025-03-01 03:50:54 -0500

    feat(obj): initialize `ObjMesh` struct and allocate initial buffers

--- src/libobj.asm
+++ src/libobj.asm
@@ -18,6 +18,7 @@
 ; ================[ EXTERN ]=============== ;
 extern fopen
 extern fclose
+extern malloc
 
 ; ===============[ INCLUDES ]============== ;
 %include "includes/cdef.inc"
@@ -26,6 +27,17 @@ extern fclose
 
 ; ================[ LOCALS ]=============== ;
 deflocal file_handle, 8
+deflocal obj_model,   8
+
+; ================[ CONSTS ]=============== ;
+%define INITIAL_VERTICES 1024
+%define INITIAL_TEXTURES 1024
+%define INITIAL_NORMALS  1024
+%define INITIAL_FACES    1024
+%define INITIAL_OBJECTS  8
+%define INITIAL_GROUPS   32
+
+%define FLOAT_SIZE       4
 
 ; =================[ DATA ]================ ;
 section .data
@@ -56,20 +68,70 @@ parse_obj_model:
     push            r15
     push            rbp
 
-    prolog          6, 8
+    prolog          6, 16
 
     ; Open .obj file
     mov             arg(2), MODE_READ
     call            fopen
     test            rax, rax
-    jz              .exit
+    jz              .exit_no_file_cleanup
     mov             [file_handle], rax
 
+    ; Allocate ObjMesh struct
+    mov             rdi, sizeof(ObjMesh)
+    call            malloc
+    test            rax, rax
+    jz              .exit
+    mov             [obj_model], rax
+
+    ; Zero out ObjMesh
+    cld
+    mov             rdi, rax
+    mov             rcx, sizeof(ObjMesh) / 8
+    xor             rax, rax
+    rep             stosq
+
+    mov             rbx, [obj_model]
+
+    ; Vertices
+    mov             arg(1), INITIAL_VERTICES * 3 * FLOAT_SIZE
+    call            malloc
+    mov             [rbx + ObjMesh.vertices], rax
+
+    ; Textures
+    mov             arg(1), INITIAL_TEXTURES * 3 * FLOAT_SIZE
+    call            malloc
+    mov             [rbx + ObjMesh.textures], rax
+
+    ; Normals
+    mov             arg(1), INITIAL_NORMALS * 3 * FLOAT_SIZE
+    call            malloc
+    mov             [rbx + ObjMesh.normals], rax
+
+    ; Faces
+    mov             arg(1), INITIAL_FACES * sizeof(ObjFace)
+    call            malloc
+    mov             [rbx + ObjMesh.faces], rax
+
+    ; Objects
+    mov             arg(1), INITIAL_OBJECTS * sizeof(ObjObject)
+    call            malloc
+    mov             [rbx + ObjMesh.objects], rax
+
+    ; Groups
+    mov             arg(1), INITIAL_GROUPS * sizeof(ObjGroup)
+    call            malloc
+    mov             [rbx + ObjMesh.groups], rax
+
+
+.exit:
     ; Close .obj file
     mov             arg(1), [file_handle]
     call            fclose
 
-.exit:
+.exit_no_file_cleanup:
+
+    mov             rax, rbx
 
     epilog
 
