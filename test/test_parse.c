674d9e4efdd70bfe9f71018cb5bce2372ae5bda6
Mark Devenyi <markdevenyidev@gmail.com>
2025-03-02 23:33:42 -0500

    test(obj): print out ObjMesh `_count` fields in `test_parse.c`

--- test/test_parse.c
+++ test/test_parse.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
+#include <stdint.h>
 
-extern void* parse_obj_model(const char* filename);
+struct ObjMesh {
+    uint64_t vertex_count;
+    uint64_t normal_count;
+    uint64_t texture_count;
+    uint64_t face_count;
+    uint64_t line_count;
+    uint64_t group_count;
+    uint64_t object_count;
+    uint64_t material_count;
+    float* vertices;
+    float* normals;
+    float* textures;
+    void* faces;
+    void* lines;
+    void* groups;
+    void* objects;
+    void* materials;
+};
+
+extern struct ObjMesh* parse_obj_model(const char* filename);
 
 int main() {
     printf("=====>  test_parse.c\n");
-    void* retval = parse_obj_model("samples/cube.obj");
+    struct ObjMesh* retval = parse_obj_model("samples/cube.obj");
     if (retval == NULL) {
         printf("[FAIL]  Test failed\n");
     } else {
+        printf("[    ]  Vertex count....%lu\n", retval->vertex_count);
+        printf("[    ]  Normal count....%lu\n", retval->normal_count);
+        printf("[    ]  Texture count...%lu\n", retval->texture_count);
+        printf("[    ]  Face count......%lu\n", retval->face_count);
         printf("[ OK ]  Test done\n");
     }
 
